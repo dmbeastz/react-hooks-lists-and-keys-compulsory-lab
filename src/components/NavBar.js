e8cebb863d8be1aff4557a0fa721a2bc1cbc6fc3
dmbeastz <daveroymutisya2@gmail.com>
2023-11-09 23:55:36 +0300

    Completed Lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linksties = links.map((link, id) => {
+    let ties = `#${link}`;
+    return <a key={id} href={ties}>{link}</a>;
+  });
+  return <nav>{linksties}</nav>;
 }
 
 export default NavBar;
+
